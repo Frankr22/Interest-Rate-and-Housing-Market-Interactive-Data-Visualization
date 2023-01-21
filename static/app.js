5485365ee5107df34fe1b69632a8afbda61c17c3
Robert Franklin <robertlsfranklin@gmail.com>
2023-01-21 14:58:11 +0800

    up to date changes

--- static/app.js
+++ static/app.js
@@ -1,51 +1,48 @@
 // Add event listener
 document.addEventListener("DOMContentLoaded", function(){
 
-  // Make a GET request to the Flask API endpoint
-    fetch('http://127.0.0.1:5000/api/data')
-    .then(response => response.json())
-    .then(data => {
-  // parse the json string
-    const parsedData = JSON.parse(data);
-  // Get the table body element
-    let tbody = document.querySelector('tbody');
-
-  // Clear any existing rows from the table
-    tbody.innerHTML = '';
-
-  // Initialize DataTables on the table
   $(document).ready(function() {
-    $('#data-table').DataTable({
-      "paging":   false,
-      "ordering": false,
-      "info":     false,
-      "searching": false,
-      "lengthChange": false,
-      "pageLength": 10
-    });
+      $.ajax({
+          type: "GET",
+          url: "http://127.0.0.1:5000/rba/data",
+          success: function(data) {
+              // Parse the JSON string into a JavaScript object
+              var jsonData = JSON.parse(data);
+              // Initialize the DataTable
+              var table = $("#rba-data-table").DataTable({
+                  scrollY: "200px",
+                  order: [[1, 'desc']],
+                  data: jsonData,
+                  columns: [
+                      { data: "date"},
+                      { data: "change_pct" },
+                      { data: "cash_rate_pct" }
+                  ]
+
+              });
+          }
+      });
   });
 
-  // Iterate through the data and create a new row for each item
-  parsedData.forEach(item => {
-    let row = document.createElement('tr');
-
-    // Create a new cell for each data point
-    let date = document.createElement('td');
-    let change = document.createElement('td');
-    let cashRate = document.createElement('td');
-
-    // Set the cell's text content to the data
-    date.textContent = new Date(item.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"});
-    change.textContent = item.change_pct;
-    cashRate.textContent = item.cash_rate_pct;
-
-    // Append the cells to the row
-    row.appendChild(date);
-    row.appendChild(change);
-    row.appendChild(cashRate);
-
-      // Append the row to the table body
-      tbody.appendChild(row);
+    $(document).ready(function() {
+      $.ajax({
+          type: "GET",
+          url: "http://127.0.0.1:5000/abs/data",
+          success: function(data) {
+              // Parse the JSON string into a JavaScript object
+              var jsonData = JSON.parse(data);
+              // Initialize the DataTable
+              var table = $("#abs-data-table").DataTable({
+                  scrollY: "200px",
+                  order: [[1, 'desc']],
+                  data: jsonData,
+                  columns: [
+                      { data: "time_period" },
+                      { data: "mean_price" }
+                  ]
+              });
+          }
+      });
   });
 
   // Define the data for the chart
@@ -79,7 +76,6 @@ document.addEventListener("DOMContentLoaded", function(){
     data[0].x.push(date);
     data[0].y.push(cashRate);
   });
-
   // Create the chart
   Plotly.newPlot('line-chart', data, layout);
 
@@ -126,6 +122,5 @@ document.addEventListener("DOMContentLoaded", function(){
 
       }
     }).addTo(map);
-});
 });
 });
