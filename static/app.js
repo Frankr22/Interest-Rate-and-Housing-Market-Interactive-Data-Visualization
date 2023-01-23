a3e0c37717400e16e2a9b47952e9f79b44a0064a
Robert Franklin <robertlsfranklin@gmail.com>
2023-01-23 16:59:33 +0800

    added calc and updated JS and HTML

--- static/app.js
+++ static/app.js
@@ -1,90 +1,100 @@
 // Add event listener
 document.addEventListener("DOMContentLoaded", function(){
 
-  // Make a GET request to the Flask API endpoint
-    fetch('http://127.0.0.1:5000/api/data')
-    .then(response => response.json())
-    .then(data => {
-  // parse the json string
-    const parsedData = JSON.parse(data);
-  // Get the table body element
-    let tbody = document.querySelector('tbody');
+    $(document).ready(function() {
+      $.ajax({
+          type: "GET",
+          url: "http://127.0.0.1:5000/rba/data",
+          success: function(data) {
+              // Parse the JSON string into a JavaScript object
+              var jsonData = JSON.parse(data);
+              // Initialize the DataTable
+              var table = $("#rba-data-table").DataTable({
+                  scrollY: "200px",
+                  data: jsonData,
+                  order: [[ 0, "desc" ]],
+                  searching: false,
+                  columns: [
+                      { data: "date"},
+                      { data: "change_pct" },
+                      { data: "cash_rate_pct" },
+                  ]
+              });
+          }
+      });
+  });
 
-  // Clear any existing rows from the table
-    tbody.innerHTML = '';
-
-  // Initialize DataTables on the table
   $(document).ready(function() {
-    $('#data-table').DataTable({
-      "paging":   false,
-      "ordering": false,
-      "info":     false,
-      "searching": false,
-      "lengthChange": false,
-      "pageLength": 10
-    });
+      $.ajax({
+          type: "GET",
+          url: "http://127.0.0.1:5000/abs/data",
+          success: function(data) {
+              // Parse the JSON string into a JavaScript object
+              var jsonData = JSON.parse(data);
+              // Initialize the DataTable
+              var table = $("#abs-data-table").DataTable({
+                  scrollY: "200px",
+                  data: jsonData,
+                  order: [[ 0, "desc" ]],
+                  searching: false,
+                  columns: [
+                      { data: "time_period" },
+                      { data: "mean_price" },
+                  ]
+              });
+          }
+      });
   });
 
-  // Iterate through the data and create a new row for each item
-  parsedData.forEach(item => {
-    let row = document.createElement('tr');
+    // // Get the data from both tables
+    // var rbaTableData = document.querySelectorAll('#rba-data-table tbody tr');
+    // var absTableData = document.querySelectorAll('#abs-data-table tbody tr');
 
-    // Create a new cell for each data point
-    let date = document.createElement('td');
-    let change = document.createElement('td');
-    let cashRate = document.createElement('td');
+    // // Extract the date and cash rate data
+    // Array.from(rbaTableData).sort((a, b) => new Date(a.children[0].textContent) - new Date(b.children[0].textContent)).forEach(function(row) {
+    //   var date = new Date(row.children[0].textContent);
+    //   var cashRate = parseFloat(row.children[2].textContent);
+    //   // Add the cash rate data to the chart
+    //   data[0].x.push(date);
+    //   data[0].y.push(cashRate);
+    // });
+    
+    // // Extract the date and house price data
+    // Array.from(absTableData).sort((a, b) => new Date(a.children[0].textContent) - new Date(b.children[0].textContent)).forEach(function(row) {
+    //   var date = new Date(row.children[0].textContent);
+    //   var housePrice = parseFloat(row.children[1].textContent);
+    //  // Add the data to the chart
+    //   data.push({
+    //   x: [date],
+    //   y: [housePrice],
+    //   type: 'line',
+    //   name: 'House Price'
+    // });
 
-    // Set the cell's text content to the data
-    date.textContent = new Date(item.date).toLocaleDateString("en-US", {year: "numeric", month: "short", day: "numeric"});
-    change.textContent = item.change_pct;
-    cashRate.textContent = item.cash_rate_pct;
+    // // Define the chart layout
+    // var layout = {
+    //   xaxis: {
+    //       type: 'date',
+    //       rangeslider: {
+    //           visible: true
+    //       }
+    //   },
+    //   yaxis: {
+    //       title: 'Cash Rate (%)'
+    //   },
+    //   yaxis2: {
+    //     title: 'House Price (AUD)',
+    //     overlaying: 'y',
+    //     side: 'right',
+    //     showgrid: false
+    //   },
+    // };
 
-    // Append the cells to the row
-    row.appendChild(date);
-    row.appendChild(change);
-    row.appendChild(cashRate);
-
-      // Append the row to the table body
-      tbody.appendChild(row);
-  });
-
-  // Define the data for the chart
-  var data = [{
-    x: [], // X-axis labels (dates)
-    y: [], // Y-axis data (cash rate)
-    type: 'line'
-  }];
-
-  // Define the layout for the chart
-  var layout = {
-    xaxis: {
-        type: 'date',
-        rangeslider: {
-            visible: true
-        }
-    },
-    yaxis: {
-        title: 'Cash Rate (%)'
-    }
-  };
-
-  // Get the data from the table
-  var tableData = document.querySelectorAll('table tbody tr');
-
-  // Extract the date and cash rate data
-  Array.from(tableData).sort((a, b) => new Date(a.children[0].textContent) - new Date(b.children[0].textContent)).forEach(function(row) {
-    var date = new Date(row.children[0].textContent);
-    var cashRate = parseFloat(row.children[2].textContent);
-    // Add the data to the chart
-    data[0].x.push(date);
-    data[0].y.push(cashRate);
-  });
-
-  // Create the chart
-  Plotly.newPlot('line-chart', data, layout);
+    // Create the chart
+    // Plotly.newPlot('line-chart', data, layout);
 
   // Initialize the map
-  var map = L.map('map').setView([-25, 135], 4);
+  var map = L.map('map').setView([-25, 135], 5);
 
   // Add the tile layer to the map
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -126,6 +136,5 @@ document.addEventListener("DOMContentLoaded", function(){
 
       }
     }).addTo(map);
-});
 });
 });
